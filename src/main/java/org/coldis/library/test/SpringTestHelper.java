7d5d92485cdd7bcaa7e191d767ce3545fa5bfd2c
rvcoutinho <me@rvcoutinho.com>
2024-10-07 11:11:43 -0300

    - Clearing context after class;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/test/SpringTestHelper.java
+++ src/main/java/org/coldis/library/test/SpringTestHelper.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 
 /**
  * Spring test helper.
  */
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class SpringTestHelper extends TestHelper {
 
 	/**
