8fe155fe333d33cf23a97ca0ab4229925dee706c
rvcoutinho <me@rvcoutinho.com>
2025-03-31 18:16:28 -0300

    - upgrading Artemis;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/test/TestHelper.java
+++ src/main/java/org/coldis/library/test/TestHelper.java
@@ -163,7 +163,7 @@ public class TestHelper {
 	 */
 	@SuppressWarnings("resource")
 	public static GenericContainer<?> createArtemisContainer() {
-		return new GenericContainer<>("coldis/infrastructure-messaging-service:2.26")
+		return new GenericContainer<>("coldis/infrastructure-messaging-service:2.27")
 				.withCreateContainerCmdModifier(cmd -> cmd.getHostConfig().withCpuCount(TestHelper.DEFAULT_CPU_QUOTA)
 						.withMemory(TestHelper.DEFAULT_MEMORY_QUOTA).withDiskQuota(TestHelper.DEFAULT_DISK_QUOTA))
 				.withExposedPorts(8161, 61616).withEnv(Map.of("ARTEMIS_USERNAME", TestHelper.TEST_USER_NAME, "ARTEMIS_PASSWORD", TestHelper.TEST_USER_PASSWORD,
