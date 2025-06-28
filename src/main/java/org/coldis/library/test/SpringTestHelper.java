8c79b063a323dcf720f5d4004edac25998387aa4
rvcoutinho <me@rvcoutinho.com>
2025-06-28 12:05:56 -0300

    - Fixing dirties context; Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/test/SpringTestHelper.java
+++ src/main/java/org/coldis/library/test/SpringTestHelper.java
@@ -11,7 +11,7 @@ import org.springframework.test.annotation.DirtiesContext;
 /**
  * Spring test helper.
  */
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class SpringTestHelper extends TestHelper {
 
 	/**
